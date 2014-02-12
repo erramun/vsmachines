ffc98242c6d7b387f7f92dc5822c7b1e30043d22
Apraiz <aapraiz@kiro-robotics.com>
2014-02-12 15:10:25 +0100

    #1

--- GitTest/frmMainGitTest.vb
+++ GitTest/frmMainGitTest.vb
@@ -5,7 +5,7 @@
     End Sub
 
     Private Sub btnSaludar_Click(sender As Object, e As EventArgs) Handles btnSaludar.Click
-        MsgBox("Hello Pepito concentrado")
+        MsgBox("Hello man")
     End Sub
 
     Private Sub btnPresentarse_Click(sender As Object, e As EventArgs) Handles btnPresentarse.Click
